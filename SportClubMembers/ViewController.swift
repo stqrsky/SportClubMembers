ca112ce4ee5ad2ecbed7f9bd21165d7efaa6b15c
Starsky <starsky.m@live.com>
2022-02-05 19:33:06 +0100

    tableView optional basic build

--- SportClubMembers/ViewController.swift
+++ SportClubMembers/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var persons = ["Starsky", "Annika", "Silvi!", "Alwin", "Ramzi", "Janina", "Baris"]
+    
+    //MARK: - Outlet
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
 
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return persons.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = persons[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}
+    
+
+extension ViewController: UITableViewDelegate {
+    
+}
